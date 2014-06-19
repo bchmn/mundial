c0c1e6eeca671b9c23122ffc8f9fecf33b92f2dc
Jonathan Bachman <jbachman@gmail.com>
2014-06-19 19:07:03 +0300

    removed db use guid for slide file name

--- controllers/slides.js
+++ controllers/slides.js
@@ -1,6 +1,7 @@
 var env = process.env.NODE_ENV || 'development',
 	config = require('../config/environments')[env],
-	db = require('mongoskin').db(config.app.db);
+	guid = require('guid');
+	//db = require('mongoskin').db(config.app.db);
 
 exports.create = function(req, res) {
     if (!req.body || !req.body.hasOwnProperty('image_data_uri')) {
@@ -16,25 +17,20 @@ exports.create = function(req, res) {
             res.send({ error: true , message: 'invalid image_data_uri' });
           }
           else {
-            db.collection('slides').insert({}, function(err, result) {
-                if (err) throw err;
-                if (result) {                  
-                  var slideId = result[0]._id.toString();
-                  console.error('creating slide ' + slideId);
-                  var imageFileName = slideId + '.jpg';
-                  fs.writeFile(__dirname + '/../public/slides/' + imageFileName , new Buffer(matches[2], 'base64'), function(err) {
-                   if (err) {
-                     db.collection('slides').remove({_id: slideId});
-                     console.error(err);
-                     res.send({ error: true , message: 'error writing image' });
-                   }
-                   else {
-                      var slideURL = req.protocol + '://' + req.get('host') + '/slides/' + imageFileName;
-                      res.send({error: false, image_url: slideURL });                      
-                   }
-                });
-                }
-            });
+              var slideId = guid.raw();
+	          console.log('creating slide ' + slideId);
+	          var imageFileName = slideId + '.jpg';
+	          fs.writeFile(__dirname + '/../public/slides/' + imageFileName , new Buffer(matches[2], 'base64'), function(err) {
+	           if (err) {
+	             db.collection('slides').remove({_id: slideId});
+	             console.error(err);
+	             res.send({ error: true , message: 'error writing image' });
+	           }
+	           else {
+	              var slideURL = req.protocol + '://' + req.get('host') + '/slides/' + imageFileName;
+	              res.send({error: false, image_url: slideURL });                      
+	           }
+	        });	
           }
       }
 };
